8ef0641fff71bdec17af970da327dc08be261598
kenual <kenneth_lau@yahoo.com>
2024-05-26 04:18:24 -0700

    Add example of llm.chat()

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from llama_index.core.llms import ChatMessage
 from llama_index.llms.gpt4free import GPT4Free
 
 llm = GPT4Free(g4f_model="gpt-4")
@@ -10,3 +11,13 @@ for token in response:
     print(token.delta, end="")
 print()
 print(llm.get_provider())
+print()
+
+messages = [
+    ChatMessage(role="system", content="you are an English to French translator"),
+    ChatMessage(role="user", content="hello there"),
+    ChatMessage(role="assistant", content="Bonjour!"),
+    ChatMessage(role="user", content="What is your name?"),
+]
+response = llm.chat(messages)
+print(response)
