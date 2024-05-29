c7fab3012eec4ee28fbe0d8425bc6395e0a28e3d
kenual <kenneth_lau@yahoo.com>
2024-05-29 02:32:45 -0700

    Add custom agent function example

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from llama_index.core.llms import ChatMessage
+from llama_index.core.tools import FunctionTool
 from llama_index.llms.gpt4free import GPT4Free
+from llama_index.core.agent import ReActAgent
+import llama_index.core
+
+llama_index.core.set_global_handler("simple")
 
 llm = GPT4Free(g4f_model="gpt-4")
 
 messages = [
-    ChatMessage(role="system", content="you are an English to French translator"),
+    ChatMessage(role="system",
+                content="you are an English to French translator"),
     ChatMessage(role="user", content="hello there"),
     ChatMessage(role="assistant", content="Bonjour!"),
     ChatMessage(role="user", content="What is your name?"),
@@ -12,11 +18,26 @@ messages = [
 response = llm.chat(messages)
 print(response)
 
+# define sample Tool
+
+
+def smart_combine(a: int, b: int) -> int:
+    """smart combine two integers and returns the resulting integer"""
+    return a * b + a + b
+
+
+multiply_tool = FunctionTool.from_defaults(fn=smart_combine)
+
+# initialize ReAct agent
+agent = ReActAgent.from_tools([multiply_tool], llm=llm, verbose=True)
+response = agent.chat("What is the result of smart combining 123 and 321 ?")
+print(response, end="\n\n")
+
 while True:
     user_input = input(f"> ")
     if user_input.lower() in ['', 'exit', 'quit']:
         break
-    
+
     response = llm.stream_complete(user_input)
 
     print()
@@ -24,4 +45,3 @@ while True:
         print(token.delta, end="")
     print(end="\n\n")
     print(llm.get_provider())
-
